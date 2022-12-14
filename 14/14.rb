0977022643898ec33dca3788da5957bedcccda67
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2022-12-14 20:03:47 +0100

    Use array instead of set making the constant ~20 times smaller

--- 14/14.rb
+++ 14/14.rb
@@ -1,7 +1,8 @@
 require 'set'
 
 Point = Struct.new(:y, :x)
-@grid = Set[]
+@points = Set.new
+@grid = []
 
 @sands = 0
 
@@ -15,10 +16,10 @@ def add_points(segments)
 		succ = segments[idx + 1]
 		if current.y != succ.y
 			min, max = [current.y, succ.y].minmax
-			min.upto(max) { @grid << Point.new(_1, current.x)}
+			min.upto(max) { @points << Point.new(_1, current.x) }
 		else
 			min, max = [current.x, succ.x].minmax
-			min.upto(max) { @grid << Point.new(current.y, _1)}
+			min.upto(max) { @points << Point.new(current.y, _1) }
 		end
 	end
 	segments.each { @max_y = [@max_y, _1.y].max }
@@ -30,22 +31,20 @@ def simulate_till_end
 		x = 500
 		while true
 			if y >= @max_y + 1
-				point = Point.new(y, x)
-				@grid << point
+				@grid[y][x] = true
 				@sands += 1
 				break
 			end
-			if !@grid.include?(Point.new(y + 1, x))
+			if !@grid[y + 1][x]
 				y += 1
-			elsif !@grid.include?(Point.new(y + 1, x - 1))
+			elsif !@grid[y + 1][x - 1]
 				y += 1
 				x -= 1
-			elsif !@grid.include?(Point.new(y + 1, x + 1))
+			elsif !@grid[y + 1][x + 1]
 				y += 1
 				x += 1
 			else
-				point = Point.new(y, x)
-				@grid << point
+				@grid[y][x] = true
 				@sands += 1
 				return @sands if y == 0
 				break
@@ -56,4 +55,9 @@ end
 File.readlines('in.txt').each do |line|
 	add_points(line.split(" -> ").map { Point.new(*_1.split(",").map(&:to_i).reverse) })
 end
+
+@grid = Array.new(@max_y + 2) { Array.new(500 + @max_y + 2, false) }
+@points.each { @grid[_1.y][_1.x] = true }
+@points = Set.new
+
 p simulate_till_end
